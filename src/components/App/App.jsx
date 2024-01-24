c66326f714552a4f8bc9684c33f042a39db17b79
SaumyaPatil <saupatil10dec@gmail.com>
2024-01-24 20:41:52 +0530

    modified:   public/index.html 	deleted:    src/App.css 	deleted:    src/App.js 	deleted:    src/App.test.js 	new file:   src/assets/flash.png 	new file:   src/assets/logo.png 	new file:   src/components/App/App.css 	new file:   src/components/App/App.jsx 	deleted:    src/index.css 	modified:   src/index.js 	deleted:    src/logo.svg 	deleted:    src/reportWebVitals.js 	deleted:    src/setupTests.js

---
+++ src/components/App/App.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+class App extends React.Component{
+    render(){
+        return (
+            <h1>Hey there! My name is Saumya!</h1>
+        )
+    }
+}
+
+export default App;
