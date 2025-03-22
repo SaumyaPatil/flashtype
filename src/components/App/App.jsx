9f3ed8b40e723f10ce96a25986668c8df8d696a6
SaumyaPatil <saupatil10dec@gmail.com>
2025-03-22 11:10:57 +0530

    Working with UI

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -62,58 +62,13 @@ class App extends React.Component{
         }, 1000);
     }; 
 
-    // startTimer() {
-    //     this.setState({ timeStarted: true });
-
-    //     const timer = setInterval(() => {
-    //         this.setState(prevState => ({
-    //             timeRemaining: prevState.timeRemaining > 0 ? prevState.timeRemaining - 1 : 0
-    //         }));
-
-    //         if (this.state.timeRemaining <= 0) {
-    //             clearInterval(timer);
-    //         }
-    //     }, 1000);
-    // }
-
-    // startTimer = () =>{
-    //     this.setState({timeStarted: true});
-    //     const timer = setInterval(()=>{
-    //         this.setState((prevState) => {
-    //             if (prevState.timeRemaining > 0) {
-    //                 return {
-    //                     timeRemaining: prevState.timeRemaining - 1,
-    //                 };
-    //             } else {
-    //                 clearInterval(timer);
-    //                 return { timeStarted: false };
-    //             }
-    //         });
-    //     }, 1000.0);
-    // };
-
+    
     startAgain = () => {
         this.fetchNewParagraph();
     }
 
-    //This below function triggers any other input or timer as soon as the user starts typing.
     handleUserInput = (inputValue) =>{
         if(!this.state.timeStarted) this.startTimer();
-
-        /* 
-         * 1. Handle the underflow case - all characters should be shown as unattempted
-         * 2. Handle the overflow case - early exit
-         * 3. Handle the backspace
-                - Mark the index+1 as not attempted (irrespective of whether the index is 0)
-                - But, dont forget to check for the overflow case here
-                (index+1 -> out of bound, when index === length - 1)
-         * 4. Update the status in the test info
-                - Find out the last character in the inputValue and its index 
-                - Check if the character at the same index in testInfo (state) matches or not
-                - Yes -> correct
-                - No -> incorrect
-         * 5. Irrespective of the case, characters, words, and speed (wpm) can be updated
-        */
 
         const characters = inputValue.length;
         const words = inputValue.split(" ").length;
@@ -138,18 +93,14 @@ class App extends React.Component{
             return;
         }
 
-        //Make a copy of testInfo
         const testInfo = this.state.testInfo;
         if(!(index === this.state.selectedParagraph.length - 1))
             testInfo[index + 1].status = "notAttempted";
 
-        //Check for the correct typed letter
         const isCorrect = inputValue[index] === testInfo[index].testLetter;
 
-        //Update the testInfo
         testInfo[index].status = isCorrect ? "correct" : "incorrect";
 
-        //Update the state
         this.setState({
             testInfo,
             words,
